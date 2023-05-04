b38841e484f6761816345d3f57dfd6ab618a45de
AlexOsuna48 <118821738+AlexOsuna48@users.noreply.github.com>
2023-05-04 09:25:36 +0200

    suma y resta

--- src/main/java/com/mycompany/sumaresta/SumaResta.java
+++ src/main/java/com/mycompany/sumaresta/SumaResta.java
@@ -7,4 +7,11 @@ public class SumaResta {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    public static int suma(int num1, int num2){
+        return num1 + num2;
+    }
+     public static int resta(int num1, int num2){
+        return num1 - num2;
+    }
 }
